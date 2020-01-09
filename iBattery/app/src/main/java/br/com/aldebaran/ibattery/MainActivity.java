b9b3d6abf3b6c8709e6caf68c1bbe46e27dc2053
Álefe de Lima Moreira <delimaalefe@gmail.com>
2020-01-09 18:51:40 -0300

    feat(java): alarme(switch) e isAlarme created

--- iBattery/app/src/main/java/br/com/aldebaran/ibattery/MainActivity.java
+++ iBattery/app/src/main/java/br/com/aldebaran/ibattery/MainActivity.java
@@ -3,8 +3,12 @@ package br.com.aldebaran.ibattery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch alarme = (Switch) findViewById(R.id.alarme);
+    boolean isAlarme = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
