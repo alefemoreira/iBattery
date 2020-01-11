99396f869524924fe2a05f830d8738a75ac2fb96
Álefe de Lima Moreira <delimaalefe@gmail.com>
2020-01-10 23:10:10 -0300

    feat(mainactivity.java): created getBatteryLevel()
    the function getBatteryLevel() was created and return the battery level

--- iBattery/app/src/main/java/br/com/aldebaran/ibattery/MainActivity.java
+++ iBattery/app/src/main/java/br/com/aldebaran/ibattery/MainActivity.java
@@ -1,46 +1,19 @@
 package br.com.aldebaran.ibattery;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+//import android.content.res.Resources;
+import android.os.BatteryManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
-import android.view.ContextThemeWrapper;
-import android.view.Display;
+//import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,21 +31,37 @@ public class MainActivity extends AppCompatActivity {
         this.moldBattery = findViewById(R.id.batteryMold);
         this.batteryFill = findViewById(R.id.fillBattery);
         this.test = findViewById(R.id.teste);
+        this.percentage = findViewById(R.id.percentage);
         alarme.setChecked(isAlarme);
 
-//        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-//        Intent batteryStatus = context.registerReceiver(null, ifilter);
-//        System.out.println(batteryStatus);
     }
+
+    public float getBatteryLevel(){
+        try {
+            IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus = registerReceiver(null, ifilter);
+
+            int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+            float batteryPct = level / (float) scale;
+
+
+            return batteryPct;
+        } catch (Exception e){
+            System.err.println("Erro ao pegar a % da bateria:" + e);
+            return 0F;
+        }
+    }
+
     public void setIsAlarme(View view){
         this.isAlarme = this.alarme.isChecked();
-        final Resources.Theme theme = getResources().newTheme();
-
         if (isAlarme == false) {
             this.test.setText("No");
             this.connectedBackground.setBackgroundResource(R.drawable.ic_bgconnectedred);
-
-        } else{
+            float bPercentage = getBatteryLevel() * 100;
+            this.percentage.setText(Math.round(bPercentage) + "%");
+        } else {
             this.test.setText("Yes");
             this.connectedBackground.setBackgroundResource(R.drawable.ic_bgconnectedgreen);
         }
