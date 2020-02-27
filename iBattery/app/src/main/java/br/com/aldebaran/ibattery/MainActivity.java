9352df9040ab84e1860edef097b296bcebb95f54
Álefe de Lima Moreira <delimaalefe@gmail.com>
2020-02-27 18:38:38 -0300

    feat(notification): notification whenever the battery is in 100%
    now, whenever the battery is in 100%, the app will send a notification, alerting the user
    
    BREAKING CHANGE: Full battery notification
    
    Closes #8

--- iBattery/app/src/main/java/br/com/aldebaran/ibattery/MainActivity.java
+++ iBattery/app/src/main/java/br/com/aldebaran/ibattery/MainActivity.java
@@ -1,16 +1,20 @@
 package br.com.aldebaran.ibattery;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -21,13 +25,15 @@ import java.util.Scanner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity implements OnEventChangedListener {
-    public Switch alarme;
+
+    public static final String BATTERY_NOTIFICATION_ID = "888";
+
+    public Switch alarm;
     public ImageView connectedBackground, moldBattery, batteryFill;
-    public TextView test, percentage, batteryText, toAlarm, connected;
-    public boolean isAlarme = true;
+    public TextView actionPowerText, percentage, batteryText, toAlarm, connected;
+    public boolean isAlarm;
 
     private PowerConnectionReceiver pcr = new PowerConnectionReceiver();
     private BatteryPercentageReceiver bp = new BatteryPercentageReceiver();
@@ -39,16 +45,14 @@ public class MainActivity extends AppCompatActivity implements OnEventChangedLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.alarme = findViewById(R.id.alarme);
+        this.alarm = findViewById(R.id.alarme);
         this.connectedBackground = findViewById(R.id.bgConnected);
         this.moldBattery = findViewById(R.id.batteryMold);
         this.batteryFill = findViewById(R.id.fillBattery);
-        this.test = findViewById(R.id.teste);
+        this.actionPowerText = findViewById(R.id.actionPowerText);
         this.percentage = findViewById(R.id.percentage);
-        alarme.setChecked(isAlarme);
 
         iBatteryJson = new File(getExternalFilesDir(null), "iBattery.json");
-
 
         registerReceiver(pcr, new IntentFilter(Intent.ACTION_POWER_CONNECTED));
         registerReceiver(pcr, new IntentFilter(Intent.ACTION_POWER_DISCONNECTED));
@@ -63,11 +67,11 @@ public class MainActivity extends AppCompatActivity implements OnEventChangedLis
             }
         }
 
-        alarme.setChecked(verifyAlarmState());
+        alarm.setChecked(verifyAlarmState());
+        isAlarm = verifyAlarmState();
 
         setPercentageText();
         setConnectedText();
-
     }
 
     @Override
@@ -101,10 +105,22 @@ public class MainActivity extends AppCompatActivity implements OnEventChangedLis
     }
 
     @Override
-    public void onConnectionChanged() { setConnectedText(); }
+    public void onConnectionChanged() {
+        setConnectedText();
+
+        if (getBatteryLevel() == 100 && isCharging()) {
+            createNotification();
+        }
+    }
 
     @Override
-    public void onPercentageChanged() { setPercentageText(); }
+    public void onPercentageChanged() {
+        setPercentageText();
+
+        if (getBatteryLevel() == 100 && isCharging()) {
+            createNotification();
+        }
+    }
 
     public float getBatteryLevel(){
         try {
@@ -142,16 +158,16 @@ public class MainActivity extends AppCompatActivity implements OnEventChangedLis
     }
 
     public void setIsAlarme(View view){
-        this.isAlarme = this.alarme.isChecked();
-        writeAlarmState(this.isAlarme);
+        this.isAlarm = this.alarm.isChecked();
+        writeAlarmState(this.isAlarm);
     }
 
     public  void setConnectedText(){
         if (isCharging()){
-            this.test.setText(R.string.teste_positive);
+            this.actionPowerText.setText(R.string.teste_positive);
             this.connectedBackground.setBackgroundResource(R.drawable.ic_bgconnectedgreen);
         } else {
-            this.test.setText(R.string.teste_negative);
+            this.actionPowerText.setText(R.string.teste_negative);
             this.connectedBackground.setBackgroundResource(R.drawable.ic_bgconnectedred);
         }
     }
@@ -161,16 +177,14 @@ public class MainActivity extends AppCompatActivity implements OnEventChangedLis
         Intent batteryStatus = registerReceiver(null, ifilter);
 
         int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL; //Bateria cheia
 
-        return isCharging;
+        return status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
     }
 
     public boolean verifyAlarmState(){
         JSONObject iBattery = readFileJSON(iBatteryJson);
         try {
-            Log.i("Script", iBattery.get("isAlarm").toString());
             return Boolean.parseBoolean(iBattery.get("isAlarm").toString());
         } catch (JSONException json) {
             json.printStackTrace();
@@ -206,5 +220,33 @@ public class MainActivity extends AppCompatActivity implements OnEventChangedLis
             json.printStackTrace();
             return new JSONObject();
         }
+    }
+
+    public void createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(BATTERY_NOTIFICATION_ID, "Bateria", importance);
+            channel.setDescription(String.valueOf(R.string.channel_description));
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void createNotification(){
+        createNotificationChannel();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, BATTERY_NOTIFICATION_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Bateria Carregada")
+                .setContentText("Sua bateria está completamente carregada")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManagerCompat.notify(999, builder.build()); //999
     }
 }
--- iBattery/app/src/main/java/br/com/aldebaran/ibattery/PowerConnectionReceiver.java
+++ iBattery/app/src/main/java/br/com/aldebaran/ibattery/PowerConnectionReceiver.java
@@ -1,8 +1,15 @@
 package br.com.aldebaran.ibattery;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import static androidx.core.content.ContextCompat.getSystemService;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
 
